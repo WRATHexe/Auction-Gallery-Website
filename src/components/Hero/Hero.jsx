83ab5169b289cef9bf33013fefa485f34e7c5fee
WRATHeXe <FardinRuhian001@gmail.com>
2025-04-11 13:07:37 +0600

    added navbar and hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div
+            className="hero h-[730px] w-[1820px] justify-start"
+            style={{
+                backgroundImage: "url('../src/assets/Banner-min.jpg')",
+            }}>
+            {/* <div className="hero-overlay"></div> */}
+            <div className="hero-content flex-col mx-36 max-w-2xl items-start">
+                    <h1 className="text-5xl font-bold text-white">Discover Treasures from Every Corner</h1>
+                    <p className="my-5 text-xl opacity-80 text-white">
+                        Explore a world of unique finds, from antique artifacts to modern masterpieces. Whether you're searching for a rare gem or a timeless classic, our auctions offer something special for everyone.
+                    </p>
+                    <button className="btn btn-primary text-black bg-white rounded-full border-0">Explore Auctions</button>
+
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
